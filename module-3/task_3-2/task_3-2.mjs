0a6c622c97c2f2f6e4c16b45887368ae1df38586
KittyCookies <eeskaar@uia.no>
2025-11-27 15:07:25 +0100

    Progress on 3.2

--- module-3/task_3-2/task_3-2.mjs
+++ module-3/task_3-2/task_3-2.mjs
@@ -3,51 +3,85 @@ import { printOut, newLine } from "../../common/script/utils.mjs";
 
 printOut("--- Part 1 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+let txtCountUp = "";
+
+for (let i = 1; i <= 10; i++){ //counting up
+  txtCountUp += " " + i.toString();
+}  
+
+let txtCountDown = "";
+
+for (let i = 10; i > 0; i--){ //counting down
+  txtCountDown += " " + i.toString();
+}
+printOut(txtCountUp + newLine + txtCountDown + newLine);
 printOut(newLine);
 
 printOut("--- Part 2 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+/* Guess number game */
+
+let Number = Math.floor(Math.random() * 60) + 1;
+while (Number !== 21){
+  Number = Math.floor(Math.random() * 60) + 1;
+}
+
+printOut("The number is " + Number);
 printOut(newLine);
 
 printOut("--- Part 3 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+let CorrectNumber = Math.floor(Math.random() * 1000000) + 1;
+while (CorrectNumber !== 21){
+  CorrectNumber = Math.floor(Math.random() * 100) + 1; //Change it to 1 000 000 after figuring out stuff
+  printOut("The number is " + CorrectNumber);
+}
+printOut(newLine)
+printOut("I guessed it right!")
 printOut(newLine);
 
 printOut("--- Part 4 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+/* Find all prime numbers between 2 and 200*/ //since 1 is not a prime number!
+
+for (let n = 2; n <= 200; n++) {
+
+  let d = 2;
+  let isPrime = true;  // assume prime until proven otherwise
+
+  while (d < n){
+
+    /*check*/ if (n % d ==0) {
+      isPrime = false; //found a not prime
+      break; // stop checking
+    }
+    d++; // try next divisor
+  }
+  if (isPrime) {
+    printOut(n);
+  }
+}
+
 printOut(newLine);
 
 printOut("--- Part 5 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
+
+
+
 printOut("Replace this with you answer!");
 printOut(newLine);
 
 printOut("--- Part 6 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
+
+
+
 printOut("Replace this with you answer!");
 printOut(newLine);
 
 printOut("--- Part 7 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
-
-printOut("--- Part 8 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
-
-printOut("--- Part 9 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
-
-/* Task 10*/
-printOut("--- Part 10 ---------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
 printOut("Replace this with you answer!");
 printOut(newLine);
