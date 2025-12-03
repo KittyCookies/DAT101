91663afa24f8bd3efaa49426d1549322c0a2cd0a
KittyCookies <eeskaar@uia.no>
2025-12-03 10:47:08 +0100

    3.2 sorta finished

--- module-3/task_3-2/task_3-2.mjs
+++ module-3/task_3-2/task_3-2.mjs
@@ -34,7 +34,7 @@ printOut("--- Part 3 -----------------------------------------------------------
 
 let CorrectNumber = Math.floor(Math.random() * 1000000) + 1;
 while (CorrectNumber !== 21){
-  CorrectNumber = Math.floor(Math.random() * 100) + 1; //Change it to 1 000 000 after figuring out stuff
+  CorrectNumber = Math.floor(Math.random() * 50) + 1; //Change it to 1 000 000 after figuring out stuff
   printOut("The number is " + CorrectNumber);
 }
 printOut(newLine)
@@ -67,21 +67,69 @@ printOut(newLine);
 
 printOut("--- Part 5 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
+//For loop within a for loop, for this code.
 
+let TableP5 = "";
+for (let row = 1; row <= 7; row++) {
+  let line5 = "";
 
+  for (let col = 1; col <= 10; col++) {
+    line5 += "K" + col + "R" + row + " ";
+  }
 
-printOut("Replace this with you answer!");
+  TableP5 += line5 + newLine;
+}
+printOut(TableP5);
 printOut(newLine);
 
-printOut("--- Part 6 ----------------------------------------------------------------------------------------------");
+printOut("--- Part 6 ---------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
 
+for(let i = 0; i < 5; i++){
+  let score = Math.floor(Math.random() * 236) + 1;
+  let percent = (score / 236) * 100;
 
+  if (percent >= 89) {
+  printOut("The Student's scores are in. The grade is A, you passed.")
+}
+else if (percent >= 77) {
+  printOut("The Student's scores are in. The grade is B, you passed.")
+}
+else if (percent >= 65) {
+  printOut("The Student's scores are in. The grade is C, you passed.")
+}
+else if (percent >= 53) {
+  printOut("The Student's scores are in. The grade is D, you passed.")
+}
+else if (percent >= 41) {
+  printOut("The Student's scores are in. The grade is E, you barely passed.")
+}
+else {
+  printOut("The Student's scores are in. The grade is F, get out.")
+}
 
-printOut("Replace this with you answer!");
+}
+
 printOut(newLine);
 
 printOut("--- Part 7 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+for(let i > 5; i < 5; i++) {
+
+  Math.floor(Math.random() * 6) + 1;
+
+  let throws = 0;
+  let done = false;
+
+  if (!done){
+    throws++;
+
+    // roll your 6 dice here
+    // check if the condition is met
+    // if yes → done = true
+  }
+ //I couldn't figure this out..
+}
+
 printOut(newLine);
